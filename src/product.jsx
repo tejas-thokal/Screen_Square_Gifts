8480a91b11b9668b1e9828f0d9b197a59ab51748
Tejas Thokal <tejasthokal2305@gmail.com>
2025-05-26 20:31:36 +0530

    Fix: Update flask set import to use FlaskSet1.webp

--- src/product.jsx
+++ src/product.jsx
@@ -4,7 +4,7 @@ import "./product.css";
 import tshirt from "./converted/Combine.webp";
 import Mug from "./converted/Mug4.webp";
 import Hoodie from "./converted/Hoodie.webp";
-import Flaskset from "./converted/FlaskSet.webp";
+import Flaskset from "./converted/FlaskSet1.webp";
 import Cap from "./converted/Cap.webp";
 import Wallet from "./converted/Wallet.webp";
 import Bottle from "./converted/Bottle.webp";
