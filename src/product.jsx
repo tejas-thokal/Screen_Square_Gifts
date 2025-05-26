81a9d79cc6bf82d1c62273f93bd3e696a3f83544
Tejas Thokal <tejasthokal2305@gmail.com>
2025-05-26 20:29:56 +0530

    Fix: Update all image imports to use correct case in product.jsx

--- src/product.jsx
+++ src/product.jsx
@@ -4,10 +4,10 @@ import "./product.css";
 import tshirt from "./converted/Combine.webp";
 import Mug from "./converted/Mug4.webp";
 import Hoodie from "./converted/Hoodie.webp";
-import Flaskset from "./converted/flaskset.webp";
-import Cap from "./converted/cap.webp";
+import Flaskset from "./converted/FlaskSet.webp";
+import Cap from "./converted/Cap.webp";
 import Wallet from "./converted/Wallet.webp";
-import Bottle from "./converted/bottle.webp";
+import Bottle from "./converted/Bottle.webp";
 import Set from "./converted/Set.webp";
 
 const products = [
